bdb15fd4883cdca4a8ede82581c5bf573241782a
simonvik <simon@devsn.se>
2013-09-30 20:26:24 +0200

    Somewhat done

--- client/background.js
+++ client/background.js
@@ -1,14 +1,17 @@
 var ipstuff = {
-	ip2long : function(ip){
-		var ippart = ip.split(".");
-		return (parseInt(ippart[0]) << 24 | parseInt(ippart[1]) << 16 | parseInt(ippart[2]) << 8 | parseInt(ippart[3]));
-	},
+        ip2long : function(ip){
+                var ippart = ip.split(".");
+                return (parseInt(ippart[0]) *  16777216 + parseInt(ippart[1]) * 65536 + parseInt(ippart[2]) * 256 + parseInt(ippart[3]));
+        },
 
-	contains_within : function(ip, cidr){
-		
+        contains_within : function(ip, cidr){
+                var parts = cidr.split('/');
+                var mask = (~((1 << (32 - parts[1])) - 1)) ;        
+                return (((ipstuff.ip2long(ip) & mask) >>> 0) == ((ipstuff.ip2long(parts[0]) & mask) >>> 0));
+        }
 
-	}
 }
+
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 	chrome.tabs.query({active:true, currentWindow:true},function(tab){
@@ -20,14 +23,23 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 		xhr.onreadystatechange = function() {
 			if (xhr.readyState == 4) {
 				var a = JSON.parse(xhr.response);
+				var h = request.hosts;
+
+				for(var item in h) {
+					var ips = h[item].ip_range;
+					for(var ip in ips){
+						if(ipstuff.contains_within(a.ip, ips[ip])){
+							chrome.pageAction.setIcon({tabId: sender.tab.id, path: 'icons/' + h[item].icon});
+							chrome.pageAction.setTitle({tabId: sender.tab.id, title: h[item].url});
+							chrome.pageAction.show(sender.tab.id);
+						}
+
+					}
+				}
 			}
 		}
 		xhr.send();
 
-
-		chrome.pageAction.setIcon({tabId: sender.tab.id, path: 'icons/one.ico'});
-		chrome.pageAction.setTitle({tabId: sender.tab.id, title: "test"});
-		chrome.pageAction.show(sender.tab.id);
 
 	});
 	sendResponse({});
